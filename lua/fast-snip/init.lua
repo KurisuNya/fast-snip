ee27fddccc3f685bec3fab408285b8843ea2ef67
KurisuNya <cuizhu1216@gmail.com>
2023-11-15 21:49:03 +0800

    feat: 修改默认文件夹

--- lua/fast-snip/init.lua
+++ lua/fast-snip/init.lua
@@ -6,80 +6,21 @@ local format_session = require("fast-snip.lib.format-session").FormatSession
 -- a line match the regex, so plugin can add the snippet
 -- to the correct position
 local defaults_options = {
-	snippets_dir = nil, -- use plugin snippets dir
-	template_file_path = nil, -- use plugin defaults template
+	snippets_dir = nil, -- use defaults dir vim.fn.stdpath("data") .. "/fast-snip/snippets"
+	template_file_path = nil, -- use plugin defaults template file
+	regex = [[-\+ generated snippets]], -- regex pattern
 	format_config = nil, -- see "fast-snip.lib.format-session"
-	regex = [[-\+ generated snippets]],
 }
+
 local snippets_dir = nil
 local current_session = nil
 local template_file_path = nil
-
-local write_snippet_to_file = function(file_type, snippet_string)
-	-- pre check
-	if vim.fn.isdirectory(snippets_dir) == 0 then
-		utils.notify("Can't find snippets dir.", vim.log.levels.ERROR)
-		return
-	end
-	if vim.fn.filereadable(template_file_path) == 0 then
-		utils.notify("can't read template file.", vim.log.levels.ERROR)
-		return
-	end
-	-- read old snippet file
-	local file_path = snippets_dir .. "/" .. file_type .. ".lua"
-	local old_file = nil
-	local created_new_file = false
-	if vim.fn.filereadable(file_path) == 0 then
-		-- create snippet file form template
-		old_file = vim.fn.readfile(template_file_path)
-		if vim.fn.writefile(old_file, file_path) == -1 then
-			utils.notify("Can't create snippet file.", vim.log.levels.ERROR)
-			return
-		end
-		created_new_file = true
-	end
-	old_file = old_file or vim.fn.readfile(file_path)
-	-- find regex.
-	local regex = vim.regex(defaults_options.regex)
-	local target_line = nil
-	for line_number, line in ipairs(old_file) do
-		if regex:match_str(line) then
-			target_line = line_number
-			break
-		end
-	end
-	if not target_line then
-		utils.notify("Snippet file format illegal.", vim.log.levels.ERROR)
-		return
-	end
-	-- generate new file
-	local new_file = {}
-	for line_number, line in ipairs(old_file) do
-		if line_number == target_line then
-			for _, snippet_line in ipairs(vim.split(snippet_string, "\n")) do
-				table.insert(new_file, snippet_line)
-			end
-		end
-		table.insert(new_file, line)
-	end
-	if vim.fn.writefile(new_file, file_path) == -1 then
-		utils.notify("Can't write snippet to file.", vim.log.levels.ERROR)
-		return
-	end
-	utils.notify("Creating new snippet success.", vim.log.levels.INFO)
-	-- reload snippet file
-	if created_new_file then
-		require("luasnip.loaders.from_lua").lazy_load({ paths = snippets_dir })
-	else
-		require("luasnip.loaders").reload_file(vim.fn.expand(file_path)) -- hot reloading with LuaSnip
-	end
-end
 
 local M = {}
 M.reload_snippets = function()
 	-- check snippets dir
 	if vim.fn.isdirectory(snippets_dir) == 0 then
-		utils.notify("Can't find snippets dir.", vim.log.levels.ERROR)
+		utils.notify(utils.info.snip_dir_error, vim.log.levels.ERROR)
 		return
 	end
 	-- reload all snippets in dir
@@ -88,21 +29,32 @@ M.reload_snippets = function()
 		require("luasnip.loaders").reload_file(path)
 	end
 end
+
 M.new_snippet_or_add_placeholder = function()
 	-- create new snippet
 	if not current_session then
 		current_session = format_session:new(defaults_options.format_config)
-		utils.notify("Creating new snippet...", vim.log.levels.INFO)
+		utils.notify(utils.info.session_create_info, vim.log.levels.INFO)
 	-- add placeholder
 	else
 		current_session:add_placeholder()
-		utils.notify("Add snippet placeholder success.", vim.log.levels.INFO)
+		utils.notify(utils.info.placeholder_add_info, vim.log.levels.INFO)
 	end
 end
+
 M.finalize_snippet = function()
+	-- pre check
+	if vim.fn.isdirectory(snippets_dir) == 0 then
+		utils.notify(utils.info.snip_dir_error, vim.log.levels.ERROR)
+		return
+	end
+	if vim.fn.filereadable(template_file_path) == 0 then
+		utils.notify(utils.info.template_file_error, vim.log.levels.ERROR)
+		return
+	end
 	-- detect session
 	if not current_session then
-		utils.notify("Can't find snippets dir.", vim.log.levels.ERROR)
+		utils.notify(utils.info.snip_empty_error, vim.log.levels.ERROR)
 		return
 	end
 	-- create snippet
@@ -111,25 +63,37 @@ M.finalize_snippet = function()
 		if trigger then
 			current_session:set_trigger(trigger)
 			local snippet_string = current_session:produce_final_snippet()
-			write_snippet_to_file(file_type, snippet_string)
+			local info, log_level = utils.write_snippet_to_file(
+				snippets_dir,
+				template_file_path,
+				defaults_options.regex,
+				file_type,
+				snippet_string
+			)
 			current_session = nil
+			utils.notify(info, log_level)
 		else
-			utils.notify("Trigger word can't be empty.", vim.log.levels.ERROR)
+			utils.notify(utils.info.trigger_empty_error, vim.log.levels.ERROR)
 		end
 	end)
 end
+
 M.setup = function(opts)
 	-- read user opts
 	defaults_options = vim.tbl_deep_extend("force", defaults_options, opts or {})
 	snippets_dir = defaults_options.snippets_dir or utils.get_snippets_dir()
 	template_file_path = defaults_options.template_file_path or utils.get_template_path()
-	snippets_dir = vim.fs.normalize(snippets_dir)
-	template_file_path = vim.fs.normalize(template_file_path)
-	-- load snippets
+	-- pre check
 	if vim.fn.isdirectory(snippets_dir) == 0 then
-		utils.notify("Can't find snippets dir.", vim.log.levels.ERROR)
+		utils.notify(utils.info.snip_dir_error, vim.log.levels.ERROR)
 		return
 	end
+	if vim.fn.filereadable(template_file_path) == 0 then
+		utils.notify(utils.info.template_file_error, vim.log.levels.ERROR)
+		return
+	end
+	snippets_dir = vim.fs.normalize(snippets_dir)
+	template_file_path = vim.fs.normalize(template_file_path)
 	require("luasnip.loaders.from_lua").lazy_load({ paths = snippets_dir })
 end
 return M
--- lua/fast-snip/lib/utils.lua
+++ lua/fast-snip/lib/utils.lua
@@ -1,31 +1,102 @@
+---@diagnostic disable: undefined-field
 local Utils = {}
+
+Utils.info = {
+	snip_dir_error = "Can't find snippets dir.",
+	template_file_error = "Can't read template file.",
+	file_illegal_error = "Can't find line match regex.",
+	file_create_error = "Can't create snippet file.",
+	snip_create_error = "Can't write snippet to file.",
+	snip_empty_error = "Can't create empty snippet.",
+	trigger_empty_error = "Can't add empty trigger.",
+	session_create_info = "Creating new snippet...",
+	snip_create_info = "Create new snippet success.",
+	placeholder_add_info = "Add snippet placeholder success.",
+}
 
 Utils.notify = function(msg, log_level)
 	vim.notify(msg, log_level, { title = "Fast Snip" })
 end
 
-Utils.get_template_path = function()
-	local runtime_paths = vim.api.nvim_list_runtime_paths()
-	for _, path in ipairs(runtime_paths) do
-		local from
-		from, _ = path:find("fast%-snip")
-		if from then
-			return path .. "/lua/fast-snip/template"
+Utils.get_snippets_dir = function()
+	local snippest_dir = vim.fn.expand(vim.fn.stdpath("data") .. "/fast-snip/snippets")
+	if vim.fn.isdirectory(snippest_dir) == 0 then
+		if vim.fn.mkdir(snippest_dir, "p") == 0 then
+			return nil
 		end
 	end
-	return nil
+	return snippest_dir
 end
 
-Utils.get_snippets_dir = function()
+Utils.get_template_path = function()
 	local runtime_paths = vim.api.nvim_list_runtime_paths()
-	for _, path in ipairs(runtime_paths) do
-		local from
-		from, _ = path:find("fast%-snip")
-		if from then
-			return path .. "/lua/fast-snip/snippets"
+	local template_path = vim.fn.expand(vim.fn.stdpath("data") .. "/fast-snip/template")
+	-- copy template in plugin to data folder
+	if vim.fn.filereadable(template_path) == 0 then
+		local plugin_template_path = nil
+		for _, path in ipairs(runtime_paths) do
+			local from
+			from, _ = path:find("fast%-snip")
+			if from then
+				plugin_template_path = vim.fn.expand(path .. "/lua/fast-snip/template")
+				break
+			end
+		end
+		if not plugin_template_path or vim.fn.filereadable(plugin_template_path) == 0 then
+			return nil
+		end
+		local template_file = vim.fn.readfile(plugin_template_path)
+		if vim.fn.writefile(template_file, template_path) == -1 then
+			return nil
 		end
 	end
-	return nil
+	return template_path
+end
+
+Utils.write_snippet_to_file = function(snippets_dir, template_file_path, regex, file_type, snippet_string)
+	-- read old snippet file
+	local file_path = snippets_dir .. "/" .. file_type .. ".lua"
+	local old_file = nil
+	local created_new_file = false
+	if vim.fn.filereadable(file_path) == 0 then
+		-- create snippet file form template
+		old_file = vim.fn.readfile(template_file_path)
+		if vim.fn.writefile(old_file, file_path) == -1 then
+			return Utils.info.file_create_error, vim.log.levels.ERROR
+		end
+		created_new_file = true
+	end
+	old_file = old_file or vim.fn.readfile(file_path)
+	-- find regex.
+	local target_line = nil
+	for line_number, line in ipairs(old_file) do
+		if vim.regex(regex):match_str(line) then
+			target_line = line_number
+			break
+		end
+	end
+	if not target_line then
+		return Utils.info.file_illegal_error, vim.log.levels.ERROR
+	end
+	-- generate new file
+	local new_file = {}
+	for line_number, line in ipairs(old_file) do
+		if line_number == target_line then
+			for _, snippet_line in ipairs(vim.split(snippet_string, "\n")) do
+				table.insert(new_file, snippet_line)
+			end
+		end
+		table.insert(new_file, line)
+	end
+	if vim.fn.writefile(new_file, file_path) == -1 then
+		return Utils.info.snip_create_error, vim.log.levels.ERROR
+	end
+	if created_new_file then
+		require("luasnip.loaders.from_lua").lazy_load({ paths = snippets_dir })
+	else
+		require("luasnip.loaders").reload_file(vim.fn.expand(file_path)) -- hot reloading with LuaSnip
+	end
+	return Utils.info.snip_create_info, vim.log.levels.INFO
 end
 
 return Utils
